ab3dc3567db1cc1d293e1e6e12771ad6d5070c91
magnoliateam <magnoliaresource.m6@gmail.com>
2026-09-08 21:31:29 +0700

    chore: update pricing, links, and pixel settings via Admin

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /**
  * CONFIGURASI DINAMIS - BUKU #1KONTENLAGI
- * Diperbarui otomatis dari Admin Panel pada 8/9/2026, 21.31.11
+ * Diperbarui otomatis dari Admin Panel pada 8/9/2026, 21.31.27
  */
 window.SITE_CONFIG = {
   "pricing": {
@@ -9,7 +9,7 @@ window.SITE_CONFIG = {
     "mainPriceNumber": "123.456",
     "currencyPrefix": "Rp",
     "instruction": "Klik tombol dibawah untuk Order Buku ↘",
-    "btnPhysicalText": "Order Buku Fisik Sekarang",
+    "btnPhysicalText": "COBA BELI LAH",
     "btnPhysicalUrl": "https://1kontenlagi.form.id/buku--1kontenlagi-",
     "btnEbookText": "Order Versi E-Book Sekarang",
     "btnEbookUrl": "https://1kontenlagi.form.id/buku--1kontenlagi-"
